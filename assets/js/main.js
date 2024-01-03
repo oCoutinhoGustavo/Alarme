671449839d998bd6ea0a0dbb36e0588343c66211
oCoutinhoGustavo <gustavomota555@gmail.com>
2024-01-03 05:50:27 -0300

    fix: finalização

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,12 @@
 const relogio = document.querySelector('#relogio')
 const form = document.querySelector('#form')
+const pararAlarme = document.querySelector('#pararAlarmeHTML')
 let horaDoAlarme
+const audio = new Audio('./assets/audio/alarme.mp3');
+let reset = String(0).padStart(2, '0')
 
-const rel = () => {
+
+const rel = (tempo) => {
   const data = new Date;
   const horas = String(data.getHours()).padStart(2, '0');
   const minutos = String(data.getMinutes()).padStart(2, '0');
@@ -11,7 +15,12 @@ const rel = () => {
   relogio.innerHTML = `${horas}:${minutos}:${segundos}`
 
   if (`${horaDoAlarme}:00` == `${horas}:${minutos}:${segundos}`) {
+  
     console.log('oioi');
+    const btn = criaBtnParaSom()
+    pararAlarme.appendChild(btn)
+
+    audio.play()
   }
 }
 setInterval(rel, 1000);
@@ -21,31 +30,64 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
   const inputHoras = document.querySelector('#horas')
   const inputMinutos = document.querySelector('#minutos')
-  const horas = String(inputHoras.value).padStart(2, '0')
-  const minutos = String(inputMinutos.value).padStart(2, '0')
+  const horas = inputHoras.value;
+  const minutos = inputMinutos.value;
 
-  if (horas.length <= 2 && minutos.length <= 2) {
-    const p = criaP(); 
-    const btn = criaBtn();
-    horaDoAlarme = `${horas}:${minutos}`
-    inputHoras.value = '';
-    inputMinutos.value = '';
-    p.innerText = horaDoAlarme
-    btn.id
-    p.appendChild(btn)
-    form.appendChild(p)
+  const seuAlarme = document.querySelector('#seuAlarme')
 
-  } else{
-    alert('Insira uma hora válida121')
+  if (seuAlarme) {
+    seuAlarme.remove()
   }
+
+  const p = criaP();
+  const btn = criaBtn();
+  horaDoAlarme = `${horas}:${minutos}`
+
+  inputHoras.value = reset;
+  inputMinutos.value = reset;
+  p.innerHTML = `<br>Seu alarme está para:<br> <b>${horaDoAlarme}</b><br>`
+  p.appendChild(btn)
+  form.appendChild(p)
+
+
 })
 
 const criaP = () => {
   const p = document.createElement('p')
+  p.id = 'seuAlarme'
   return p
 }
 
 const criaBtn = () => {
   const btn = document.createElement('button')
+  btn.innerText = 'remover alarme'
+  btn.id = 'btnRemover'
+  btn.type = 'button';
   return btn
 }
+const criaBtnParaSom = () => {
+  const btn = document.createElement('button')
+  btn.innerText = 'Para alarme'
+  btn.id = 'pararAlarme'
+  btn.type = 'button';
+  return btn
+}
+
+
+document.addEventListener('click', (e) => {
+  const click = e.target
+
+  if (click.id == 'btnRemover') {
+    click.parentElement.remove()
+    horaDoAlarme = ''
+  }
+
+  if (click.id == 'pararAlarme') {
+    const seuAlarme = document.querySelector('#seuAlarme')
+    seuAlarme.remove();
+    click.remove()
+
+    audio.pause();
+    audio.currentTime = 0;
+  }
+})
